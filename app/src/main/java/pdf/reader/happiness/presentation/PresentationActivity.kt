773585ac3e760847293fe702c9744744bf3dd038
xaldarof <temuruz2003@gmail.com>
2021-10-03 15:52:32 +0500

    changed style

---
+++ app/src/main/java/pdf/reader/happiness/presentation/PresentationActivity.kt
@@ -0,0 +1,32 @@
+package pdf.reader.happiness.presentation
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import org.koin.core.component.KoinApiExtension
+import pdf.reader.happiness.MainActivity
+import pdf.reader.happiness.R
+
+class PresentationActivity : AppCompatActivity() {
+    @KoinApiExtension
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_presentation)
+        supportActionBar?.hide()
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2500)
+
+    }
+}
