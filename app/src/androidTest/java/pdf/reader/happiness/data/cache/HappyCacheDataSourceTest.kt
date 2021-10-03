1e9bbb7e09768ef7eab341205642e83f3cf75b19
xaldarof <temuruz2003@gmail.com>
2021-10-03 13:32:55 +0500

    [data-cache] - unit tested

---
+++ app/src/androidTest/java/pdf/reader/happiness/data/cache/HappyCacheDataSourceTest.kt
@@ -0,0 +1,46 @@
+package pdf.reader.happiness.data.cache
+
+import androidx.lifecycle.asLiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.junit.Assert.*
+import org.junit.Test
+import org.koin.core.component.KoinApiExtension
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import pdf.reader.happiness.data.models.Type
+import pdf.reader.happiness.data.room.dao.CoreDao
+
+@KoinApiExtension
+class HappyCacheDataSourceTest:KoinComponent {
+
+    private val dao : CoreDao by inject()
+
+    /**
+     * checked when actual size of data was 13 [HAPPY]
+     */
+
+    @Test
+    fun check_is_db_returns_data(){
+        CoroutineScope(Dispatchers.Main).launch {
+            dao.fetchHappy(Type.HAPPY).asLiveData().observeForever {
+                assertEquals(13,it.size)
+            }
+        }
+    }
+
+    @Test
+    fun check_is_db_returns_only_happy_type_data(){
+        CoroutineScope(Dispatchers.Main).launch {
+            dao.fetchHappy(Type.HAPPY).asLiveData().observeForever { it ->
+                it.forEach {
+                    assertEquals(Type.HAPPY,it.type)
+                    assertNotEquals(Type.LIFE,it.type)
+                    assertNotEquals(Type.LOVE,it.type)
+                    assertNotEquals(Type.SUCCESS,it.type)
+                }
+            }
+        }
+    }
+}
---
+++ app/src/androidTest/java/pdf/reader/happiness/data/cache/LifeCacheDataSourceTest.kt
@@ -0,0 +1,47 @@
+package pdf.reader.happiness.data.cache
+
+import androidx.lifecycle.asLiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.junit.Assert.*
+import org.junit.Test
+import org.koin.core.component.KoinApiExtension
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import pdf.reader.happiness.data.models.Type
+import pdf.reader.happiness.data.room.dao.CoreDao
+
+@KoinApiExtension
+class LifeCacheDataSourceTest:KoinComponent{
+
+    private val dao:CoreDao by inject()
+
+    /**
+     *  checked when actual size of data was 38
+     */
+
+    @Test
+    fun check_is_db_returns_data(){
+        CoroutineScope(Dispatchers.Main).launch {
+            dao.fetchLife(Type.LIFE).asLiveData().observeForever {
+                assertEquals(38,it.size)
+            }
+        }
+    }
+
+    @Test
+    fun check_is_db_returns_only_life_type_data(){
+        CoroutineScope(Dispatchers.Main).launch {
+            dao.fetchLife(Type.LIFE).asLiveData().observeForever { it ->
+                it.forEach {
+                    assertEquals(Type.LIFE,it.type)
+
+                    assertNotEquals(Type.HAPPY,it.type)
+                    assertNotEquals(Type.LOVE,it.type)
+                    assertNotEquals(Type.SUCCESS,it.type)
+                }
+            }
+        }
+    }
+}
