bab9a704ee6e3daaa46bab69749ab0a2172b3b80
xaldarof <temuruz2003@gmail.com>
2021-10-20 23:18:25 +0500

    coin transaction(token)

--- app/src/main/java/pdf/reader/happiness/core/CloudResult.kt
+++ app/src/main/java/pdf/reader/happiness/core/CloudResult.kt
@@ -3,6 +3,7 @@ package pdf.reader.happiness.core
 
 class CloudResult<out T>(val data: T?, val status: Status) {
 companion object {
+
     fun <T> success(data: T, status: Status): CloudResult<T> {
         return CloudResult(data, status)
     }
@@ -11,8 +12,8 @@ companion object {
         return CloudResult(data, status)
     }
 }
-
 }
+
 enum class Status{
     ERROR,
     SUCCESS
